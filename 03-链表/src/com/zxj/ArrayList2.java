260dafee2b3fb1df06534ffdd339b307c8b3f640
zuojie <xijie.zuo@tcl.com>
2021-09-19 12:17:20 +0800

    动态数组的clear方法的缩容

--- 03-链表/src/com/zxj/ArrayList2.java
+++ 03-链表/src/com/zxj/ArrayList2.java
@@ -31,6 +31,9 @@ public class ArrayList2<E> extends AbstractList<E> {
 			elements[i] = null;
 		}
 		size = 0;
+		if(elements != null && elements.length > DEFAULT_CAPACITY) {
+			elements = (E[]) new Object[DEFAULT_CAPACITY];
+		}
 	}
 
 	/**
