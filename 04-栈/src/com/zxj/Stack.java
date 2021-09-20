1f60630a93d42936c25ebbff78647a16a072afd2
zuojie <xijie.zuo@tcl.com>
2021-09-20 18:23:59 +0800

    栈的实现

---
+++ 04-栈/src/com/zxj/Stack.java
@@ -0,0 +1,26 @@
+package com.zxj;
+
+import com.zxj.list.*;
+
+public class Stack<E>{
+	private List<E> list = new ArrayList<>();
+	
+	public int size() {
+		return list.size();
+	}
+	
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+	public void push(E element) {
+		list.add(element);
+	}
+
+	public E pop() {
+		return list.remove(size() -1);
+	}
+
+	public E top() {// 官方的是peek()
+		return list.get(size() -1);
+	}
+}
