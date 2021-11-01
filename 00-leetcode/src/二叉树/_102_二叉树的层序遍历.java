092f3bc49852c9c14e74a1221e5084fe481f319a
zuojie <xijie.zuo@tcl.com>
2021-11-01 15:22:11 +0800

    完成二叉树的层序遍历

--- 00-leetcode/src/二叉树/_102_二叉树的层序遍历.java
+++ 00-leetcode/src/二叉树/_102_二叉树的层序遍历.java
@@ -14,11 +14,13 @@ public class _102_二叉树的层序遍历 {
 		List<List<Integer>> res = new ArrayList<>();
 		if(root == null) return res;
 		Queue<TreeNode> queue = new LinkedList<>();
-		queue.offer(root);
+		queue.offer(root);//将根节点放入队列中，然后不断遍历队列
 		while(!queue.isEmpty()) {
 			List<Integer> level = new ArrayList<Integer>();
 			//获取当前队列的长度，这个长度相当于 当前这一层的节点个数
 			int currentLeveSize = queue.size();
+			//将队列中的元素都拿出来(也就是获取这一层的节点)，放到level的list中
+			//如果节点的左/右子树不为空，也放入队列中
 			for(int i =1;i<= currentLeveSize;i++) {
 				TreeNode node = queue.poll();
 				level.add(node.val);
@@ -29,7 +31,7 @@ public class _102_二叉树的层序遍历 {
 					queue.offer(node.right);
 				}
 			}
-			res.add(level);
+			res.add(level);//将level的list加入最终返回结果中
 		}
 		return res;
 	}
